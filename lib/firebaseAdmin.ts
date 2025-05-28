d94f5d20bce44e3e3d53f2c484961eec1af39c3b
apple <apple@apples-MacBook-Pro-4.local>
2025-05-28 18:07:34 +0530

    version 1.1

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -1,7 +1,14 @@
 import { initializeApp, App, cert, getApps, getApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { getStorage } from "firebase-admin/storage";
-var serviceKey = require("@/speech-text-speech-6b769-firebase-adminsdk-fbsvc-4ec382b9fb.json");
+// var serviceKey = require("@/speech-text-speech-6b769-firebase-adminsdk-fbsvc-4ec382b9fb.json");
+const serviceAccountJson = process.env.NEXT_PUBLIC_FIREBASE_SERVICE_ACCOUNT_JSON;
+
+if (!serviceAccountJson) {
+  throw new Error("FIREBASE_SERVICE_ACCOUNT_JSON is not defined");
+}
+
+const serviceKey = JSON.parse(serviceAccountJson);
 
 // service side
 // cert => certified
